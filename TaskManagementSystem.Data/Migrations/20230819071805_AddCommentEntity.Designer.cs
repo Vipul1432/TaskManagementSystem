6db295a0b44482ba7444ec9c826831b5e4ab0b00
Vipul <vipulupadhyay563@gmail.com>
2023-08-19 15:21:39 +0530

    Implement Users should be able to add comments to a task

--- TaskManagementSystem.Data/Migrations/20230819071805_AddCommentEntity.Designer.cs
+++ TaskManagementSystem.Data/Migrations/20230819071805_AddCommentEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskManagementSystem.Data.Context;
 
@@ -11,9 +12,11 @@ using TaskManagementSystem.Data.Context;
 namespace TaskManagementSystem.Data.Migrations
 {
     [DbContext(typeof(TaskManagementDbContext))]
-    partial class TaskManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230819071805_AddCommentEntity")]
+    partial class AddCommentEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TaskManagementSystem.Domain/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Domain.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int TaskId { get; set; }
+        public string UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
